0117ff99d3dd5d89d8d83d8cc4b3e9d2ef8f6c8a
Leana Lopez <chileana_go@yahoo.com>
2021-08-04 17:27:34 -0700

    adding static data

--- src/app.js
+++ src/app.js
@@ -2,6 +2,39 @@ const express = require("express");
 const app = express();
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
+const flips = require("./data/flips-data");
+const counts = require("./data/counts-data");
+
+app.use("/counts/:countId", (req, res, next) => {
+  const { countId } = req.params;
+  const foundCount = counts[countId];
+
+  if (foundCount === undefined) {
+    next(`Count id not found: ${countId}`);
+  } else {
+    res.json({ data: foundCount }); // Return a JSON object, not a number.
+  }
+});
+
+app.use("/counts", (req, res) => {
+  res.json({ data: counts });
+});
+
+app.use("/flips/:flipId", (req, res, next) => {
+  const { flipId } = req.params;
+  // const flipId = req.params.flipId;
+  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
+
+  if (foundFlip) {
+    res.json({ data: foundFlip });
+  } else {
+    next(`Flip id not found: ${flipId}`);
+  }
+});
+
+app.use("/flips", (req, res) => {
+  res.json({ data: flips });
+});
 
 // Not found handler
 app.use((request, response, next) => {
