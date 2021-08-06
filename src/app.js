81205f02b751b9b6ed3102f47d2db9f536d3c7d5
Leana Lopez <chileana_go@yahoo.com>
2021-08-06 14:12:35 -0700

    adding get and post handler

--- src/app.js
+++ src/app.js
@@ -5,22 +5,27 @@ const app = express();
 const flips = require("./data/flips-data");
 const counts = require("./data/counts-data");
 
-app.use("/counts/:countId", (req, res, next) => {
+app.use(express.json()); // built-in middleware that adds a body property to the request (req.body)
+
+app.get("/counts/:countId", (req, res, next) => {
   const { countId } = req.params;
   const foundCount = counts[countId];
 
   if (foundCount === undefined) {
-    next(`Count id not found: ${countId}`);
+    // next(`Count id not found: ${countId}`);
+    next({ 
+      status: 404, 
+      message: `Count id not found: ${countId}` });
   } else {
     res.json({ data: foundCount }); // Return a JSON object, not a number.
   }
 });
 
-app.use("/counts", (req, res) => {
+app.get("/counts", (req, res) => {
   res.json({ data: counts });
 });
 
-app.use("/flips/:flipId", (req, res, next) => {
+app.get("/flips/:flipId", (req, res, next) => {
   const { flipId } = req.params;
   // const flipId = req.params.flipId;
   const foundFlip = flips.find((flip) => flip.id === Number(flipId));
@@ -28,23 +33,68 @@ app.use("/flips/:flipId", (req, res, next) => {
   if (foundFlip) {
     res.json({ data: foundFlip });
   } else {
-    next(`Flip id not found: ${flipId}`);
+    // next(`Flip id not found: ${flipId}`);
+    next({ 
+      status: 404, 
+      message: `Flip id not found: ${flipId}` });
   }
 });
 
-app.use("/flips", (req, res) => {
+app.get("/flips", (req, res) => {
   res.json({ data: flips });
 });
 
+// New middleware function to validate the request body
+function bodyHasResultProperty(req, res, next) {
+  const { data: { result } = {} } = req.body;
+  if (result) {
+    return next(); // Call `next()` without an error message if the result exists
+  }
+  // next("A 'result' property is required.");
+  next({
+    status: 400,
+    message: "A 'result' property is required.",
+  });
+}
+
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
+
+app.post(
+  "/flips",
+  bodyHasResultProperty, // Add validation middleware function
+  (req, res) => {
+    // Route handler no longer has validation code.
+    const { data: { result } = {} } = req.body;
+
+    // if (result) {
+    const newFlip = {
+      id: ++lastFlipId, // Increment last ID, then assign as the current ID
+      result,
+    };
+    flips.push(newFlip);
+    counts[result] = counts[result] + 1; // Increment the counts
+    // res.json({ data: newFlip });
+    res.status(201).json({ data: newFlip });
+    // } else {
+    //   res.sendStatus(400);
+    // }
+  }
+);
+
 // Not found handler
 app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
+  // next(`Not found: ${request.originalUrl}`);
+  next({ status: 404, message: `Not found: ${request.originalUrl}` });
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  // response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
