4cb8265c36cb45ff4b5e2cd435b342793256db99
Leana Lopez <chileana_go@yahoo.com>
2021-08-07 15:34:59 -0700

    replaced params use to res.locals.flip

--- src/flips/flips.controller.js
+++ src/flips/flips.controller.js
@@ -34,6 +34,7 @@ function flipExists(req, res, next) {
   const { flipId } = req.params;
   const foundFlip = flips.find((flip) => flip.id === Number(flipId));
   if (foundFlip) {
+    res.locals.flip = foundFlip;
     return next();
   }
   next({
@@ -65,28 +66,27 @@ function list(req, res) {
 }
 
 function read(req, res) {
-  const { flipId } = req.params;
-  // const flipId = req.params.flipId;
-  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
-  res.json({ data: foundFlip });
+//   const { flipId } = req.params; // const flipId = req.params.flipId;
+//   const foundFlip = flips.find((flip) => flip.id === Number(flipId));
+res.json({ data: res.locals.flip }); // res.json({ data: foundFlip });
 }
 
 function update(req, res) {
-  const { flipId } = req.params;
-  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
-
-  const originalResult = foundFlip.result;
+//   const { flipId } = req.params;
+//   const foundFlip = flips.find((flip) => flip.id === Number(flipId));
+  const flip = res.locals.flip;
+  const originalResult = flip.result; // const originalResult = foundFlip.result;
   const { data: { result } = {} } = req.body;
 
   if (originalResult !== result) {
     // update the flip
-    foundFlip.result = result;
+    flip.result = result; //foundFlip.result = result;
     // Adjust the counts
     counts[originalResult] = counts[originalResult] - 1;
     counts[result] = counts[result] + 1;
   }
 
-  res.json({ data: foundFlip });
+  res.json({ data: flip }); //res.json({ data: foundFlip });
 }
 
 function destroy(req, res) {
